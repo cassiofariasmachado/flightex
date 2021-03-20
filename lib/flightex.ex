93ff560fee9cf7962a21aeb3c3617ce617ce4382
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2021-03-20 12:16:12 -0300

    Add moduledoc main module

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -5,13 +5,77 @@ defmodule Flightex do
   alias Flightex.Bookings.Create, as: CreateBooking
   alias Flightex.Bookings.Get, as: GetBooking
 
-  defdelegate create_user(params), to: CreateUser, as: :call
+  @doc """
+  Starts the agents.
 
-  defdelegate create_booking(user_id, params), to: CreateBooking, as: :call
-  defdelegate get_booking(booking_id), to: GetBooking, as: :call
+  ## Examples
 
+      iex> Flightex.start_agents()
+      {:ok, "Agents started"}
+
+  """
   def start_agents() do
     UserAgent.start_link(%{})
     BookingAgent.start_link(%{})
+
+    {:ok, "Agents started"}
   end
+
+  @moduledoc """
+  Documentation for `Flightex`.
+  """
+
+  @doc """
+  Create an user.
+
+  ## Examples
+
+      iex> Flightex.create_user(%{})
+      {:error, "Invalid params"}
+
+      _iex> Flightex.create_user(%{name: "Cassio", email: "example@email.com", cpf: "12345678910"})
+      {:ok, _user_id}
+
+  """
+  defdelegate create_user(params), to: CreateUser, as: :call
+
+  @doc """
+  Create a booking.
+
+  ## Examples
+
+      iex> Flightex.create_booking("invalid", %{data_completa: "2020-09-01 10:00:00",cidade_origem: "Sao Leopoldo", cidade_destino: "Porto Alegre"})
+      {:error, "User not found"}
+
+      iex> Flightex.create_booking("invalid", %{})
+      {:error, "Invalid params"}
+
+      _iex> Flightex.create_booking("07f90095-36b7-4e4c-8dca-2a0b578526c2", %{data_completa: "2020-09-01 10:00:00",cidade_origem: "Sao Leopoldo", cidade_destino: "Porto Alegre"})
+      {:ok, booking_id}
+
+  """
+  defdelegate create_booking(user_id, params), to: CreateBooking, as: :call
+
+  @doc """
+  Get a booking.
+
+  ## Examples
+
+      iex> Flightex.get_booking("1e81ab8a-e775-4b26-be80-5537fe156067")
+      {
+        :ok,
+        %Flightex.Bookings.Booking{
+          cidade_destino: "Porto Alegre",
+          cidade_origem: "São Leopoldo",
+          data_completa: ~N[2020-09-01 12:00:00],
+          id: "1e81ab8a-e775-4b26-be80-5537fe156067",
+          id_usuario: "70831e44-5820-4b4c-aae1-87b079a2882f"
+        }
+      }
+
+      iex> Flightex.get_booking("invalid")
+      {:error, "Flight Booking not found"}
+
+  """
+  defdelegate get_booking(booking_id), to: GetBooking, as: :call
 end
---
+++ test/flightex_test.exs
@@ -0,0 +1,22 @@
+defmodule FlightexTest do
+  use ExUnit.Case
+
+  alias Flightex.Bookings.Agent, as: BookingAgent
+
+  import Flightex.Factory
+
+  doctest Flightex do
+    setup %{} do
+      Flightex.start_agents()
+
+
+
+      booking_id = "1e81ab8a-e775-4b26-be80-5537fe156067"
+      booking = build(:booking, id: booking_id)
+
+      BookingAgent.save(booking)
+
+      :ok
+    end
+  end
+end
